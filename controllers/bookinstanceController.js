3639835b2c836069caca619c35f7b3c212c57e33
François ROLAND <froland@froland.be>
2020-10-19 12:00:39 +0200

    Implement bookinstance detail.

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -1,10 +1,11 @@
 const { Book, BookInstance } = require("../models/sequelize");
+const createError = require("http-errors");
 
 // Display list of all BookInstances.
 exports.bookinstance_list = async function (req, res, next) {
   try {
     const bookinstance_list = await BookInstance.findAll({
-      include: Book
+      include: Book,
     });
     res.render("bookinstance_list", {
       title: "Book Instance List",
@@ -16,8 +17,34 @@ exports.bookinstance_list = async function (req, res, next) {
 };
 
 // Display detail page for a specific BookInstance.
-exports.bookinstance_detail = function (req, res) {
-  res.send("NOT IMPLEMENTED: BookInstance detail: " + req.params.id);
+exports.bookinstance_detail = async function (req, res, next) {
+  /*
+  BookInstance.findById(req.params.id)
+    .populate('book')
+    .exec(function (err, bookinstance) {
+      if (err) { return next(err); }
+      if (bookinstance==null) { // No results.
+          var err = new Error('Book copy not found');
+          err.status = 404;
+          return next(err);
+        }
+      // Successful, so render.
+      res.render('bookinstance_detail', { title: 'Book:', bookinstance:  bookinstance});
+    })
+  */
+  try {
+    const bookinstanceId = req.params.id;
+    const bookinstance = await BookInstance.findByPk(bookinstanceId, {
+      include: Book,
+    });
+    if (bookinstance !== null) {
+      res.render("bookinstance_detail", { title: "Book:", bookinstance });
+    } else {
+      next(createError(404, "Book instance not found"));
+    }
+  } catch (error) {
+    next(error);
+  }
 };
 // Display BookInstance create form on GET.
 exports.bookinstance_create_get = function (req, res) {
