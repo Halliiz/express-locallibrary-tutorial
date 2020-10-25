120615ed8a690a88e67eb3b27a9057d355d869e2
Francois ROLAND <francois.roland@iepscf-uccle.be>
2020-10-25 19:37:50 +0100

    Update book.

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -152,11 +152,104 @@ exports.book_delete_get = function (req, res) {
 exports.book_delete_post = function (req, res) {
   res.send("NOT IMPLEMENTED: Book delete POST");
 };
+
 // Display book update form on GET.
-exports.book_update_get = function (req, res) {
-  res.send("NOT IMPLEMENTED: Book update GET");
+exports.book_update_get = async function (req, res, next) {
+  try {
+    const [book, authors, genres] = await Promise.all([
+      Book.findByPk(req.params.id, {
+        include: [Author, Genre],
+      }),
+      Author.findAll(),
+      Genre.findAll(),
+    ]);
+    if (book === null) {
+      next(createError(404, "Book not found"));
+    } else {
+      const selectedGenres = book.genres.map(genre => genre.id);
+      for (let i = 0; i < genres.length; i++) {
+        const genre = genres[i];
+        if (selectedGenres.indexOf(genre.id) > -1) {
+          genre.checked = true;
+        }
+      }
+      res.render("book_form", {
+        title: "Update Book",
+        book,
+        authors,
+        genres,
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
 };
+
 // Handle book update on POST.
-exports.book_update_post = function (req, res) {
-  res.send("NOT IMPLEMENTED: Book update POST");
-};
+exports.book_update_post = [
+  (req, res, next) => {
+    if (!(req.body.genre instanceof Array)) {
+      if (typeof req.body.genre === "undefined") {
+        req.body.genre = [];
+      } else {
+        req.body.genre = new Array(req.body.genre);
+      }
+    }
+    next();
+  },
+  body("title", "Title must not be empty.").trim().notEmpty().escape(),
+  body("author", "Author must not be empty.").trim().notEmpty().escape(),
+  body("summary", "Summary must not be empty.").trim().notEmpty().escape(),
+  body("isbn", "ISBN must not be empty").trim().notEmpty().escape(),
+  body("genre.*").escape(),
+  async (req, res, next) => {
+    try {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        const [authors, genres] = await Promise.all([
+          Author.findAll(),
+          Genre.findAll(),
+        ]);
+        for (let i = 0; i < genres.length; i++) {
+          const genre = genres[i];
+          if (req.body.genre.indexOf(genre.id.toString()) > -1) {
+            genres[i].checked = true;
+          }
+        }
+        res.render("book_form", {
+          title: "Update Book",
+          book: req.body,
+          authors,
+          genres,
+          errors: errors.array(),
+        });
+      } else {
+        const book = await Book.findByPk(req.params.id);
+        book.title =  req.body.title;
+        book.summary = req.body.summary;
+        book.isbn = req.body.isbn;
+        await book.save();
+        if (book.authorId !== req.body.author) {
+          const author = await Author.findByPk(req.body.author);
+          if (author === null) {
+            next(createError(404, "Author not found"));
+          } else {
+            await book.setAuthor(author);
+          }
+        }
+        if (req.body.genre.length > 0) {
+          const genres = await Genre.findAll({
+            where: {
+              id: req.body.genre,
+            },
+          });
+          await book.setGenres(genres);
+        }
+        res.redirect(book.url);
+      }
+    } catch (error) {
+      next(error);
+    }
+  },
+];
